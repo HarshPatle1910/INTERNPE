69e603f2e60c40a6a70ed5743f00653e8acac258
Harsh <Harsh@HarshP>
2024-07-18 00:14:21 +0530

    Game done.
    Made stone, paper, scissors game. In this user plays game with computer
    as opponent. User will get five chances to play game and win. It will
    get know about its and opponent score

--- Task_InternPe/src/Task2/Task2.java
+++ Task_InternPe/src/Task2/Task2.java
@@ -5,16 +5,79 @@
 package Task2;
 
 import java.util.Scanner;
-import java.util.InputMismatchException;
 import java.util.Random;
 
 public class Task2 {
 
 	public static void main(String[] args) {
+		
+		//Creating objects
 		Scanner sc = new Scanner(System.in);
 		Random rd = new Random();
 		
-		System.out.println("\t\t\tWelcome to Rock Paper Scissor Game");
+		//Variables
+		String[] choices = {"rock","paper","scissors"}; 
+		String pc_choice = "";
+		String user_choice = "";
+		int user_score = 0;
+		int pc_score = 0;
+		int pc_choice_number;
+		int round = 0;
+		
+		System.out.println("\t\t\tWelcome to Rock Paper Scissor Game\n");
+		System.out.println("You have 5 rounds");
+		
+		while(round<5) {
+			pc_choice_number = rd.nextInt(3);
+			pc_choice = choices[pc_choice_number];
+			
+			System.out.println("\nReady to play. This is round " + ++round);
+			
+			System.out.print("Enter your move(rock, paper, scissor): ");
+			user_choice = sc.nextLine().toLowerCase();
+			
+			if(!user_choice.equals(choices[0]) && !user_choice.equals(choices[1]) && !user_choice.equals(choices[2])) {
+				System.out.println("Invalid move! Please try again.");
+				System.out.println("To exit the game, type - 'exit'");
+				user_choice = sc.nextLine().toLowerCase();
+				if (user_choice.equals("exit")) {
+					break;
+				}
+					
+				continue;
+			}
+			
+			if (user_choice.equals(pc_choice)) {
+				System.out.println("You both choosed " + user_choice + ", it's tie.");
+			}
+			else if((user_choice.equals("rock") && pc_choice.equals("scissors")) ||
+                    (user_choice.equals("paper") && pc_choice.equals("rock")) ||
+                    (user_choice.equals("scissors") && pc_choice.equals("paper"))) {
+				user_score++;
+				System.out.println("You win this round.");
+			}
+			else {
+				pc_score++;
+				System.out.println("Computer win this round.");
+			}
+		}
+		
+		if(user_choice.equals("exit")) {
+			System.out.println("Thanks for playing");
+		}
+		else {
+			if(user_score==pc_score) {
+				System.out.println("This game is tied, you both scored: " + pc_score);
+			}
+			else if (user_score>pc_score) {
+				System.out.println("Congratulations! you win this game");
+				System.out.println("Your score: " + user_score + " and Computer score: " + pc_score);
+			}
+			else {
+				System.out.println("Oops! you lose this game");
+				System.out.println("Your score: " + user_score + " and Computer score: " + pc_score);
+			}	
+		}
 	}
 
 }
