96adf3e6fede7611260723eb979465c997a036e7
Harsh <Harsh@HarshP>
2024-07-17 13:17:38 +0530

    Task 2 started
    Task 2 : Rock Paper Scissor Game

---
+++ Task_InternPe/src/Task2/Task2.java
@@ -0,0 +1,20 @@
+/*
+ * Rock Paper Scissor Game
+ */
+
+package Task2;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Random;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rd = new Random();
+		
+		System.out.println("\t\t\tWelcome to Rock Paper Scissor Game");
+	}
+
+}
